4150869cb1fe8402152081512afcf44102d7b0aa
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-01-11 02:57:01 -0800

    Updated add function

--- add.c
+++ add.c
@@ -10,7 +10,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !*stack || !(*stack)->next)
 	{
-		dprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
