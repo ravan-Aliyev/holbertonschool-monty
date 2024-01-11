93b6bccd7bbe8ff2aee24a93056670cc071b0f34
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-01-11 03:05:28 -0800

    Bug fixed

--- swap.c
+++ swap.c
@@ -9,7 +9,7 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);		
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
