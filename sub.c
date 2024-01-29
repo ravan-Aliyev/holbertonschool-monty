1c125b89e0e7b5c4b5a5f58c3b1ceb93bfd19d80
Hamid <hemidbeyli2020@gmail.com>
2024-01-28 22:34:12 -0800

    Updated

--- sub.c
+++ sub.c
@@ -1,20 +1,34 @@
 #include "monty.h"
+
 /**
- * sub_list - subtracts the top element of the stack from the second top
- * @stack: pointer to the head of the list
- * @line_number: line number of instruction
+ * _sub - sub top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
-	stack_t *next;
+	stack_t *tmp = *stack;
+	int sub = 0, i = 0;
 
-	if (!current || !current->next)
+	if (tmp == NULL)
 	{
-		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	next = current->next;
-	next->n -= current->n;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (stack == NULL || (*stack)->next == NULL || i <= 1)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sub = (*stack)->next->n - (*stack)->n;
 	pop(stack, line_number);
+
+	(*stack)->n = sub;
 }
